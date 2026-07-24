a16e55c11d19dcec3f9099774588f90a83399cdc
Karthik <karthikpolasi1818@gmail.com>
2026-07-24 16:23:12 +0530

    Configure Prisma for Supabase production database

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,30 +2,25 @@ import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
 };
-
 
 
 const adapter = new PrismaPg({
 
-  connectionString:
-    process.env.DATABASE_URL!
+  connectionString: process.env.DATABASE_URL!,
 
 });
-
 
 
 export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
 
-globalForPrisma.prisma ??
+    adapter,
 
-new PrismaClient({
-
-  adapter
-
-});
+  });
 
 
 
